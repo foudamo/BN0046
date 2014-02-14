8ed2ca71e6ef24804989380d467dbc2b21e0c509
Ricardo Gonzalez <rickgc@gmail.com>
2014-02-14 11:57:31 +0000

    add more configuration options

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";$(function(){$('input[type="checkbox"],[type="radio"]').bootstrapSwitch()});
+"use strict";$(function(){$('input[type="checkbox"],[type="radio"]').bootstrapSwitch({onColor:"success"})});
